cc0eecd6245d917d3db19058abcf5b5a7f5fd542
Guruvel13 <letters2guruvel@gmail.com>
2025-07-09 06:26:41 +0530

    DAY-5 FN

---
+++ src/main/java/com/example/springbootfirst/config/SpringConfiguration.java
@@ -0,0 +1,11 @@
+package com.example.springbootfirst.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SpringConfiguration {
+    SecurityFilterChain securityFilterChain;
+
+}
